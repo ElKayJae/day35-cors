cf3eb4fcd96ae2c24dff89e1961c7c9257ad0f36
ElKayJae <lowkejun91@gmail.com>
2023-01-30 14:17:09 +0800

    attempt fix

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Game } from '../models/Game';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +12,8 @@ export class GameService {
   constructor(private httpClient: HttpClient) { }
 
   getGames(limit: number, offset: number): Observable<any>{
+
+    console.info("inside gameService.getGamess()")
     const queryParams = new HttpParams()
       .set("limit", limit)
       .set("offset", offset);
@@ -21,7 +22,7 @@ export class GameService {
       .set("content-type", "application/json")
       .set("Access-Control-Allow-Origin", "*")
 
-    return this.httpClient.get(this.URL, {params: queryParams, headers: headers})
+    return this.httpClient.get(this.URL, {params: queryParams, headers: headers, responseType: 'text'})
   }
 
  
